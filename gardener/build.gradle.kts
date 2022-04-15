2ed803e42d62f90757ddba1962ebb88f4306a7e9
장현애 <aeae_1@naver.com>
2022-04-15 15:30:07 +0900

    Feat: QueryDsl Setting
    드디어 ㅠ ㅠ ㅠ ㅠ

--- gardener/build.gradle.kts
+++ gardener/build.gradle.kts
@@ -7,11 +7,16 @@ plugins {
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 	kotlin("plugin.jpa") version "1.6.10"
+	kotlin("kapt") version "1.6.10"
 }
 
 group = "gdscsookmyung"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+	kotlin.srcDir("$buildDir/generated/source/kapt/main")
+}
 
 configurations {
 	compileOnly {
@@ -35,13 +40,18 @@ dependencies {
 	//spring-openapi-ui
 	implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
 
+	implementation("com.querydsl:querydsl-jpa:5.0.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("mysql:mysql-connector-java")
+
 	annotationProcessor("org.projectlombok:lombok")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
