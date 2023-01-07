383612a2778538b7051c201b9fbab2dc481d9dec
Hyeonae <aeae_1@naver.com>
2023-01-07 18:33:26 +0900

    Feat: 슬랙 메시지 보내기 기능

--- gardener/src/main/kotlin/gdscsookmyung/gardener/GardenerApplication.kt
+++ gardener/src/main/kotlin/gdscsookmyung/gardener/GardenerApplication.kt
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableJpaAuditing
+@EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties(JwtProperty::class, GitHubProperty::class, SlackProperty::class)
 class GardenerApplication
--- gardener/src/main/kotlin/gdscsookmyung/gardener/service/util/SlackUtil.kt
+++ gardener/src/main/kotlin/gdscsookmyung/gardener/service/util/SlackUtil.kt
@@ -6,7 +6,13 @@ import com.slack.api.model.Message
 import gdscsookmyung.gardener.property.SlackProperty
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.web.servlet.config.annotation.UrlBasedViewResolverRegistration
+import java.io.BufferedReader
 import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 @Service
 class SlackUtil(
@@ -26,5 +32,19 @@ class SlackUtil(
         } catch(e: SlackApiException) {
             logger.error("error: {}", e.message, e)
         }
+    }
+
+    fun sendMessage(message: String) {
+        var requestUrl = "https://slack.com/api/chat.postMessage?"
+        requestUrl += "token=" + slackProperty.token + "&"
+        requestUrl += "channel=" +  slackProperty.id
+        requestUrl += "text=" + URLEncoder.encode(message, "UTF-8")
+
+        val conn: HttpURLConnection = URL(requestUrl).openConnection() as HttpURLConnection
+        conn.setRequestProperty("Accept", "application/json")
+        conn.requestMethod = "GET"
+        conn.connect()
+
+        BufferedReader(InputStreamReader(conn.inputStream, "UTF-8"))
     }
 }
